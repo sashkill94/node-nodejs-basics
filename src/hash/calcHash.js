a181355e64405007664fd4a36d6c9f141139ba89
sashkill94 <sashkill94@gmail.com>
2023-06-19 19:03:30 +0300

    feat: solved hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const file = await readFile('./src/hash/files/fileToCalculateHashFor.txt')
+    const data = createHash('sha256').update(file).digest('hex');
+    console.log(data);
 };
 
 await calculateHash();
