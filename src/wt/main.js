9baeb0d1a4ff81f64b1f9b7a52247ecf3fc9f87a
sashkill94 <sashkill94@gmail.com>
2023-06-19 19:43:56 +0300

    feat: solved wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const calculate = (workerData) => new Promise((resolve) => {
+    const worker = new Worker('./src/wt/worker.js', {workerData});
+
+    worker.on('message', data => resolve({status: 'resolved', data}));
+    worker.on('error', data => resolve({status: 'error', data: null }));
+
+});
+
+const resultArr = [];
+
+for (let i = 0; i < cpus().length; i++) {
+    resultArr.push(calculate(10 + i));
+}
+
+const result = await Promise.all(resultArr);
+console.log(result);
 };
 
 await performCalculations();
