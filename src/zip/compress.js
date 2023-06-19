0cf5b5942367a59fbf061f3bba2fced9e50cc128
sashkill94 <sashkill94@gmail.com>
2023-06-19 19:24:30 +0300

    feat: solved zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,9 @@
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+    await pipeline(createReadStream('./src/zip/files/fileToCompress.txt'), createGzip(), createWriteStream('./src/zip/files/archive.gz'));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,9 @@
+import { pipeline } from 'stream/promises';
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    await pipeline(createReadStream('./src/zip/files/archive.gz'), createGunzip(), createWriteStream('./src/zip/files/fileToCompress.txt'));
 };
 
 await decompress();
